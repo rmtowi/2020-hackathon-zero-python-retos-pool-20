18dd96678ad712b888410c5bf0264fbfcf42a4f9
rmtowi <enrique.v.l@hotmail.com>
2020-05-16 19:53:33 +0200

    test1_fin

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,7 +7,32 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    global options
+    
+    player = player.lower()
+    ai = ai.lower()
+
+    if player == "piedra" and ai == "piedra":
+        return "Empate!"
+    elif player == "piedra" and ai == "papel":
+        return "Perdiste!"
+    elif player == "piedra" and ai == "tijeras":
+        return "Ganaste!"
+    elif player == "papel" and ai == "piedra":
+        return "Ganaste!"
+    elif player == "papel" and ai == "papel":
+        return "Empate!"
+    elif player == "papel" and ai == "tijeras":
+        return "Perdiste!"
+    elif player == "tijeras" and ai == "piedra":
+        return "Perdiste!"
+    elif player == "tijeras" and ai == "papel":
+        return "Ganaste!"
+    elif player == "tijeras" and ai == "tijeras":
+        return "Empate!"
+    else:
+        return "Error en escritura"
+
 
 # Entry Point
 def Game():
@@ -16,8 +41,14 @@ def Game():
     
     #
     #
-    
+    global options
+    player = input("Selecciona piedra, papel o tijeras: ")
+    numero_random=randint(0,2)
+    ai=options[numero_random]
+    #print("AI escogió: " + ai)
+   
     winner = quienGana(player, ai)
 
     print(winner)
 
+#Game()
